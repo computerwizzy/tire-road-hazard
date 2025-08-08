fb4739674b2290db66b99300cfbd2103b414b75f
Jaime Gonzalez <jaime@computerwizzy.com>
2025-08-08 17:45:48 +0000

    can you make it wider?

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -43,7 +43,7 @@ export default async function AdminPage() {
 
      if (error) {
          return (
-            <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+            <div className="flex-1 space-y-4 pt-6">
                  <Card>
                     <CardHeader>
                         <CardTitle>Error</CardTitle>
