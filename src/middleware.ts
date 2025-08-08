eb3be287a409d9f14b3c3695937603a902b11191
Jaime Gonzalez <jaime@computerwizzy.com>
2025-08-08 15:48:35 +0000

    when im logged and i make any menu selection do not takeme to login agai

--- src/middleware.ts
+++ src/middleware.ts
@@ -60,19 +60,30 @@ export async function middleware(request: NextRequest) {
   } = await supabase.auth.getUser()
 
   const { pathname } = request.nextUrl
-  const isAuthRoute = pathname === '/login'
-
-  if (!user && !isAuthRoute) {
+  
+  // Reroute all non-logged-in users to the login page
+  if (!user && pathname !== '/login') {
     return NextResponse.redirect(new URL('/login', request.url))
   }
-
-  if (user && isAuthRoute) {
-    return NextResponse.redirect(new URL('/admin', request.url))
+  
+  // If the user is logged in and they land on the login page, take them to the admin dashboard
+  if (user && pathname === '/login') {
+     return NextResponse.redirect(new URL('/admin', request.url))
   }
+
 
   return response
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 }
